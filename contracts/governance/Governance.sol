d2068709518f2548f72d507dd6f240e017396654
mavisges1 <241825@astanait.edu.kz>
2026-05-17 01:03:21 +0500

    final governance fixes and green test suite

--- contracts/governance/Governance.sol
+++ contracts/governance/Governance.sol
@@ -24,9 +24,9 @@ contract Governance is
     )
         Governor("RWA Governance")
         GovernorSettings(
-            1,
-            20,
-            0
+            7200,
+            50400,
+            1e18
         )
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(4)
